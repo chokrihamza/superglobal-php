73561d91cadc317249c3d1b82d43f39a884c4fef
chokrihamza <chokrihamza1994@gmail.com>
2021-06-07 13:08:42 +0200

    the get php

---
+++ get.php
@@ -0,0 +1,6 @@
+<a href="/superglobal/get.php/?name=irik&email=irkifabian4@gmail.com">get information</a>
+<?php
+
+if (isset($_GET['name'])) {
+  echo $_GET['name'];
+}
---
+++ global.php
@@ -0,0 +1,6 @@
+<?php
+$arv = $_SERVER['argv'];
+echo $_SERVER['REQUEST_METHOD'];
+echo $_SERVER['REQUEST_TIME'];
+echo $_SERVER['SERVER_NAME'];
+var_dump($arv);
