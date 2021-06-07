71f29bedf7f6f61110e3974bba973da066a57526
chokrihamza <chokrihamza1994@gmail.com>
2021-06-07 13:29:54 +0200

    the request php

---
+++ request.php
@@ -0,0 +1,9 @@
+<a href="/superglobal/request.php/?name=irik&email=irkifabian4@gmail.com">get information</a>
+<form action="" method="post">
+  <input type="text" placeholder="enter your name" name="name" />
+  <input type="email" placeholder="enter your email" name="email" />
+  <input type="password" placeholder="enter your password" name="password" />
+  <input type="submit" value="Send" name="submit" />
+</form>
+<?php
+var_dump($_REQUEST);
